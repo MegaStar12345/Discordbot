c5b9fbd6c1a9314551077c686b0d494064eadc9a
MegaStar12345 <42596751+MegaStar12345@users.noreply.github.com>
2018-09-05 12:20:09 -0500

    f
    f

--- comandos/help.js
+++ comandos/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("Este bot unicamente esta relacionado al juego **Ace of Spades**","**__Comandos__**\n\n**-server** => Para mostrar todas las estadisticas de un servidor.\n**-serverlist** => Para mostrar el serverlist.\n**-invitar** => Para que me invites a tu servidor.\n**-avatar** => Comando extra, te muestra tu avatar o el de alguien.")
   .setColor("0000FF")
   .setThumbnail(bicon)
-  await message.channel.send(botembed)
+  return message.channel.send(botembed)
 }
 
 
