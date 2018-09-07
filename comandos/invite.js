59cf0d07fe8abd09cb1638a712e0241c50e4de0f
MegaStar12345 <42596751+MegaStar12345@users.noreply.github.com>
2018-09-07 09:45:42 -0500

    sd

--- comandos/invite.js
+++ comandos/invite.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
   let botembed = new Discord.RichEmbed()
   .setTitle("Bot creado por MegaStar (MegaStar#0782)")
-  .addField("Link de invitacion","Para que me agregues a tu servidor dale click [a este mensaje :D](https://discordapp.com/oauth2/authorize?client_id=482143490558656513&scope=bot&permissions=0)")
+  .addField("Link de invitacion","Para que me agregues a tu servidor dale click [a este mensaje :D](https://discordapp.com/oauth2/authorize?client_id=482143490558656513&scope=bot&permissions=8192)")
   .setColor("0000FF")
   .setThumbnail(bicon)
   await message.author.send(botembed)
